801118aecccfb89f7b0ab6c5385385f9e884d90d
leonel fernandez <leone>
2020-05-01 11:19:19 -0400

    chart working

---
+++ src/components/ChartC.vue
@@ -0,0 +1,103 @@
+<template>
+  <div class="cnv">
+    <canvas ref="myChart" width="400" height="400"></canvas>
+  </div>
+</template>
+
+<script>
+import Chart from "chart.js";
+
+function createChart(ctx, data) {
+  const labels = data.map(d => d.Updated);
+  const confirmed = data.map(d => d.Confirmed);
+  let incr = [0];
+  for (let i = 1; i < confirmed.length; i++) {
+    incr.push(confirmed[i] - confirmed[i - 1]);
+  }
+
+  const deaths = data.map(d => d.Deaths);
+  let deathsChange = [0];
+  deaths.reduce((prev, curr) => {
+    deathsChange.push(curr - prev);
+    return curr;
+  });
+
+  const datasets = [
+    {
+      label: "Confirmed",
+      data: confirmed,
+      borderWidth: 1,
+      order: 2,
+      backgroundColor: "rgb(204, 255, 153)"
+    },
+    {
+      label: "Increment",
+      data: incr,
+      borderWidth: 1,
+      order: 1,
+      backgroundColor: "rgb(204, 204, 255)"
+    },
+    {
+      label: "Death",
+      data: deaths,
+      borderWidth: 1,
+      order: 1,
+      backgroundColor: "rgb(204, 20, 255)"
+    },
+    {
+      label: "Death Change",
+      data: deathsChange,
+      borderWidth: 1,
+      order: 1,
+      backgroundColor: "rgb(24, 204, 255)"
+    }
+  ];
+  paintChart(ctx, labels, datasets);
+}
+
+function paintChart(ctx, labels, datasets) {
+  new Chart(ctx, {
+    type: "bar",
+    data: {
+      labels,
+      datasets
+    },
+    options: {
+      maintainAspectRatio: false,
+      scales: {
+        yAxes: [
+          {
+            ticks: {
+              beginAtZero: true
+            }
+          }
+        ]
+      }
+    }
+  });
+}
+
+export default {
+  name: "ChartC",
+  props: {
+    chartData: Object
+  },
+  methods: {
+    printChart(data) {
+      const ctx = this.$refs.myChart;
+      createChart(ctx, data);
+    }
+  },
+  mounted() {
+        this.printChart(Object);
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.cnv {
+  width: 1500px;
+  height: 800px;
+}
+</style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,118 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="cnv">
+    <canvas ref="myChart" width="400" height="400"></canvas>
   </div>
 </template>
 
 <script>
+import axios from "axios";
+import Chart from "chart.js";
+
+const apiUrl = "http://localhost:3030/csv/Miami-Dade";
+
+function createChart(ctx, data) {
+  const labels = data.map(d => d.Updated);
+  const confirmed = data.map(d => d.Confirmed);
+  let incr = [0];
+  for (let i = 1; i < confirmed.length; i++) {
+    incr.push(confirmed[i] - confirmed[i - 1]);
+  }
+
+  const deaths = data.map(d => d.Deaths);
+  let deathsChange = [0];
+  deaths.reduce((prev, curr) => {
+    deathsChange.push(curr - prev);
+    return curr;
+  });
+
+  const datasets = [
+    {
+      label: "Confirmed",
+      data: confirmed,
+      borderWidth: 1,
+      order: 2,
+      backgroundColor: "rgb(204, 255, 153)"
+    },
+    {
+      label: "Increment",
+      data: incr,
+      borderWidth: 1,
+      order: 1,
+      backgroundColor: "rgb(204, 204, 255)"
+    },
+    {
+      label: "Death",
+      data: deaths,
+      borderWidth: 1,
+      order: 1,
+      backgroundColor: "rgb(204, 20, 255)"
+    },
+    {
+      label: "Death Change",
+      data: deathsChange,
+      borderWidth: 1,
+      order: 1,
+      backgroundColor: "rgb(24, 204, 255)"
+    }
+  ];
+  paintChart(ctx, labels, datasets);
+}
+
+function paintChart(ctx, labels, datasets) {
+  new Chart(ctx, {
+    type: "bar",
+    data: {
+      labels,
+      datasets
+    },
+    options: {
+      maintainAspectRatio: false,
+      scales: {
+        yAxes: [
+          {
+            ticks: {
+              beginAtZero: true
+            }
+          }
+        ]
+      }
+    }
+  });
+}
+
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
   props: {
     msg: String
+  },
+  data() {
+    return {
+      csvData: Object
+    };
+  },
+  methods: {
+    printChart(data) {
+      const ctx = this.$refs.myChart;
+      createChart(ctx, data);
+    }
+  },
+  mounted() {
+    axios
+      .get(apiUrl)
+      .then(response => response.data)
+      // .then(dt => neatCsv(dt))
+      .then(dt => {
+        this.csvData = dt;
+        this.printChart(dt);
+      });
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+.cnv {
+  width: 1500px;
+  height: 800px;
 }
 </style>
